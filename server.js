5c175b0390b3f4626e98a53facc0fb11d26a9eb2
Tpoppp <olson0002@yahoo.com>
2020-05-26 23:29:18 -0700

    condensed code and added functions

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer");
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employees_db"
+});
+
+connection.connect(function (error) {
+    if (error) {
+        throw error;
+    }
+
+  });
+
+connection.query = util.promisify(connection.query);
 
 function start() {
-  return inquirer.prompt([
+   inquirer
+   .prompt([
       {
           type: "rawlist",
           message: "What would you like to do?",
-          name: "What",
+          name: "Action",
           choices: ["View All Employees", "View All employees by Department", "View ALL employees by manager", "Add Employee", "Update Employee"]
-      },
-      {
-          type: "input",
-          message: "What would you like to do?",
-          name: "View All Employees",
-          choices: [""]
-      },
-  
-  ]).then(answers => { 
+      }]).then(function (answer) {
+        switch (answer.action) {
+          case "View All Employees":
+            employeeSearch();
+            break;
 
-  }).catch(error => {
-    if (error) {
-      console.log(error)
-    }
-  });
-};
+            case "View All employees by department":
+              departmentSearch();
+              break;
+
+              case "View All Employees By Manager":
+                managerSearch();
+        }
+      })
+    };
+
+    
+ 
+
+
+
 
 start();
